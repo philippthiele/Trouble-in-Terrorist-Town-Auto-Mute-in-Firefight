0f12078b8d79b2a5e8a21b747426e04942f4a223
Jan <jackflash5500@gmail.com>
2016-01-25 21:26:08 +0100

    Added a Mute Icon

--- lua/autorun/client/ttt_automute_client.lua
+++ lua/autorun/client/ttt_automute_client.lua
@@ -1,7 +1,27 @@
 local function MutePlayer(data)
   local playerToBeMuted = data:ReadEntity()
   playerToBeMuted:SetMuted(true)
-  timer.Create( "MuteTimer", 15, 1, function() playerToBeMuted:SetMuted(false) end )
+  if LocalPlayer() == playerToBeMuted then
+    ourMat = Material( "../gamemodes/terrortown/content/materials/vgui/ttt/glyph_muted" )
+  end
+  timer.Create( "MuteTimer", 15, 1, function()
+     playerToBeMuted:SetMuted(false)
+     ourMat = nil
+   end )
 end
 
 usermessage.Hook( "Mute Player", MutePlayer )
+
+
+
+hook.Add( "HUDPaint", "MutedIconPaint", function()
+  if ourMat then
+    if math.floor(RealTime()%2) == 0 then
+  	surface.SetDrawColor( 255, 255, 255, 255 )
+    else
+    surface.SetDrawColor( 255, 55, 55, 255 )
+    end
+  	surface.SetMaterial( ourMat	)
+  	surface.DrawTexturedRect( 10, ScrH()-210, 64, 64 )
+  end
+end )
