1de62dbf186594e694502cbf9fc175316ae3b5a4
Jan <jackflash5500@gmail.com>
2016-01-27 22:54:36 +0100

    Fixed Post Round Mute

---
+++ src/lua/autorun/client/ttt_automute_client.lua
@@ -0,0 +1,56 @@
+local allTimers = {}
+
+local function MutePlayer(data)
+	local playerToBeMuted = data:ReadEntity()
+		if not playerToBeMuted:IsMuted() then
+
+		if LocalPlayer() == playerToBeMuted then
+			ourMat = Material( "vgui/ttt/glyph_muted" )
+				if not timer.Exists("MuteTimer") then
+				timer.Create("MuteTimer", 15, 1, function()
+					ourMat = nil
+				end )		 
+			end
+		else
+		  playerToBeMuted:SetMuted(true)
+		end
+
+		local id = "MuteTimer" .. tostring(os.time())
+		local entry = {id,playerToBeMuted}
+		table.insert(allTimers, entry)
+		timer.Create(id, 15, 1, function()
+			if playerToBeMuted:IsMuted() and LocalPlayer() ~= playerToBeMuted then
+			   playerToBeMuted:SetMuted(false)	
+			   table.remove(allTimers,1)
+			end
+		 end )
+	end
+end
+
+function TTTEndRound(result)
+	for _,entry in ipairs(allTimers) do
+		timer.Adjust(entry[1], 0,1, function()
+			if entry[2]:IsMuted() and LocalPlayer() ~= entry[2] then
+			   entry[2]:SetMuted(false)	
+			   ourMat = nil			   
+			end
+		 end)
+	end
+end
+
+
+hook.Add( "TTTEndRound", "EndRoundDeleteTimers", TTTEndRound )
+
+usermessage.Hook( "Mute Player", MutePlayer )
+
+hook.Add( "HUDPaint", "MutedIconPaint", function()
+  if ourMat then
+	if math.floor(RealTime()%2) == 0 then
+	surface.SetDrawColor( 255, 255, 255, 255 )
+	else
+	surface.SetDrawColor( 255, 55, 55, 255 )
+	end
+	surface.SetMaterial( ourMat	)
+	surface.DrawTexturedRect( 10, ScrH()-210, 64, 64 )
+  end
+end )
--- src/lua/autorun/server/ttt_automute_server.lua
+++ src/lua/autorun/server/ttt_automute_server.lua
@@ -1,9 +1,12 @@
 function PlayerGetsHurt( victim, attacker )
-	if attacker:IsPlayer() then
+	if attacker:IsPlayer() and engine.ActiveGamemode() == "terrortown" then
 	  umsg.Start( "Mute Player" )
 		umsg.Entity(victim)
 	  umsg.End()
 	end
 end
 
+
 hook.Add( "PlayerHurt", "PlayerGetsHurt", PlayerGetsHurt )
+
+ 
